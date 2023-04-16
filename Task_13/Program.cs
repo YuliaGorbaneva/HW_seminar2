7783bfa4d9bcf32159e66727ca4352a1e2f7d830
Yulia <yarantseva89@bk.ru>
2023-04-16 21:54:25 +0600

    task2

---
+++ Task_13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число от 100 до 999: ");
+int number = int.Parse(Console.ReadLine());
+
+int last = number % 10;
+if (number < 100){
+    Console.WriteLine($"no third number");
+}
+else{
+    Console.WriteLine($"{last}");
+}
