171d8b62bb14e7141da691178f3f9edb99b2de66
Yulia <yarantseva89@bk.ru>
2023-04-16 23:03:17 +0600

    task3

---
+++ Task_15/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+
+string s = Console.ReadLine();
+int i = int.Parse(s);
+
+if (i == 1){
+    Console.WriteLine($"Monday, work");
+}
+else if (i == 2){
+    Console.WriteLine($"Tuesday, work");
+}
+else if (i == 3){
+    Console.WriteLine($"Wednesday, work");
+}
+else if (i == 4){
+    Console.WriteLine($"Thursday, work");
+}
+else if (i == 5){
+    Console.WriteLine($"Friday, work");
+}
+else if (i == 6){
+    Console.WriteLine($"Saturday, day off");
+}
+else if (i == 7){
+    Console.WriteLine($"Sunday, day off");
+}
+else{
+    Console.WriteLine($"not a day of the week");
+}
---
+++ Task_15v2.0/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+
+string s = Console.ReadLine();
+int i = int.Parse(s);
+
+if (i <= 5){
+    Console.WriteLine("work day");
+}
+else if (i == 6){
+    Console.WriteLine("day off");
+}
+else if (i == 7){
+    Console.WriteLine("day off");
+}
+else{
+    Console.WriteLine("not a day of the week");
+}
